baa8b24cee1b3654f816badfa1681e735f26f33c
Omar <74821022+omaraalsaied@users.noreply.github.com>
2023-09-15 21:40:40 +0300

    Update dockerizingApp.groovy

--- vars/dockerizingApp.groovy
+++ vars/dockerizingApp.groovy
@@ -3,9 +3,9 @@
 
 def call(imageName) {
      echo "building docker image .. "
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
-        sh "docker build -t $imageName ."
-        sh "echo $PWD | docker login -u $USER --password-stdin" 
-        sh "docker push $imageName"
+    // withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
+    //     sh "docker build -t $imageName ."
+    //     sh "echo $PWD | docker login -u $USER --password-stdin" 
+    //     sh "docker push $imageName"
     }
 }
