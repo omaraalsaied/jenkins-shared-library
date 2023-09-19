b169f30b84cc1db24172f9e8f8b3757551533133
Omar <74821022+omaraalsaied@users.noreply.github.com>
2023-09-19 05:45:36 +0300

    fixing the deploying to ec2

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
 
 def call() {
-    def dockerCmd = 'docker run -p8080:8080 -d --name java-app-2 omaraalsaied/java-maven-app:1.1.3-27'
     echo 'deploying the application'
+    def dockerCmd = "docker run -p8080:8080 -d  omaraalsaied/java-maven-app:${IMAGE_NAME}"
     sshagent(['ec2-server-key']) {
-        sh "ssh -o StrictHostKeyChecking ec2-user@13.38.251.191 ${dockerCmd}"
+        sh "ssh -o StrictHostKeyChecking=no ec2-user@13.38.251.191 ${dockerCmd}"
     }
  }
